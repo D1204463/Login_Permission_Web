61398bfe8ad35196b52b49fde0e4c083d85b617f
D1204493 <161418128+D1204493@users.noreply.github.com>
2024-12-10 17:26:27 +0800

    修改 SideBar.vue
    主頁面<a>拿掉

--- Login-Permission-Project-frontend/src/components/Layout/Sidebar.vue
+++ Login-Permission-Project-frontend/src/components/Layout/Sidebar.vue
@@ -32,13 +32,11 @@
                         id="menu">
                         <!-- 主頁面 -->
                         <li class="nav-item w-100">
-                            <router-link to="/">
-                                <a class="nav-link menu-link">
-                                    <div class="d-flex align-items-center">
-                                        <font-awesome-icon :icon="['fas', 'house']" size="xl" />
-                                        <span class="ms-3 d-none d-sm-inline">主頁面</span>
-                                    </div>
-                                </a>
+                            <router-link to="/" class="nav-link menu-link">
+                                <div class="d-flex align-items-center">
+                                    <font-awesome-icon :icon="['fas', 'house']" size="xl" />
+                                    <span class="ms-3 d-none d-sm-inline">主頁面</span>
+                                </div>
                             </router-link>
                         </li>
 
